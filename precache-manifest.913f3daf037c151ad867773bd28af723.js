141b82ea37d6f622303c67da5e1c465df5ab567f
bobothechimp <55031588+bobothechimp@users.noreply.github.com>
2022-10-18 15:20:05 +0000

    Deploying to master from  @ 82b41b22d6e7644036f711006bbe1c2d504b27ca 🚀

---
+++ precache-manifest.913f3daf037c151ad867773bd28af723.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5e102d52538ae9fc6906dd7e6d3c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "755c546f9740c6e2bbac",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "16cc110b8ddbd9537a7e",
+    "url": "/static/css/main.2aa3fe0b.chunk.css"
+  },
+  {
+    "revision": "755c546f9740c6e2bbac",
+    "url": "/static/js/2.d30a8136.chunk.js"
+  },
+  {
+    "revision": "e550f3b3f614afba8d67a185777c821b",
+    "url": "/static/js/2.d30a8136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cc110b8ddbd9537a7e",
+    "url": "/static/js/main.6eb3ff85.chunk.js"
+  },
+  {
+    "revision": "c34cd509c99df98b746f",
+    "url": "/static/js/runtime-main.9b2e4e3d.js"
+  },
+  {
+    "revision": "d581b6b8a226f6f624fc1855ba50cc1b",
+    "url": "/static/media/header.d581b6b8.jpg"
+  },
+  {
+    "revision": "1fa3818bc22bfe78ed940f1fefc65d75",
+    "url": "/static/media/logo.1fa3818b.png"
+  },
+  {
+    "revision": "5e504bdcd9edd9ab341b27f6b0b7308c",
+    "url": "/static/media/projects.5e504bdc.jpg"
+  }
+]);
